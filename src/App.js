56ab92fd66c2590029fb8a7d7fb50f009aa901b3
pavithra-1123 <pavithrarssp@gmail.com>
2023-11-26 14:28:20 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import { BrowserRouter, Route,  Routes } from 'react-router-dom';
 export function App() {
   return (
     <>
-     <BrowserRouter>
+    <div className='container'>
+    <BrowserRouter>
     <HomePage/>
     <Routes>
       <Route path='Home1' exact element={<Home1/>} />
       <Route path='list1' exact element={<ListPage1/>}/>
     </Routes>
     </BrowserRouter>
+    </div>
     </>
   );
 }
